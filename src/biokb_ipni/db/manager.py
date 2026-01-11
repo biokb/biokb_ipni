a87f58970136246d8099a25f4a845f3dc5bbf152
Christian Ebeling <Christian.Ebeling@scai.fraunhofer.de>
2026-01-11 18:21:17 +0100

    import data fixed

--- src/biokb_ipni/db/manager.py
+++ src/biokb_ipni/db/manager.py
@@ -2,6 +2,8 @@ import logging
 import os
 import shutil
 import sqlite3
+import urllib.request
+import zipfile
 from typing import Any, Optional
 
 import pandas as pd
@@ -10,23 +12,27 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 
 from biokb_ipni.constants import (
+    DATA_FOLDER,
     DB_DEFAULT_CONNECTION_STR,
-    DEFAULT_PATH_UNZIPPED_DATA_FOLDER,
+    DOWNLOAD_URL,
+    PATH_TO_TAXTREE_ZIP_FILE,
+    PATH_TO_ZIP_FILE,
+    RANKED_LINEAGE_COLUMNS,
+    RANKED_LINEAGE_DTYPES,
+    TAXTREE_DATA_FOLDER,
+    TAXTREE_DOWNLOAD_URL,
     TsvFileName,
 )
 from biokb_ipni.db.models import (
     Base,
+    Family,
     Name,
     NameRelation,
     Reference,
-    Taxon,
     TypeMaterial,
+    Location,
 )
-from biokb_ipni.tools import (
-    download_and_unzip,
-    get_cleaned_and_standardized_dataframe,
-    parse_date,
-)
+from biokb_ipni.tools import get_cleaned_and_standardized_dataframe, parse_date
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -46,7 +52,7 @@ def set_sqlite_pragma(
 file_table_map: dict[str, Any] = {
     TsvFileName.REFERENCE: Reference,
     TsvFileName.NAME: Name,
-    TsvFileName.TAXON: Taxon,
+    TsvFileName.TAXON: Family,
     TsvFileName.TYPE_MATERIAL: TypeMaterial,
     TsvFileName.NAMES_RELATION: NameRelation,
 }
@@ -60,24 +66,22 @@ class DbManager:
     def __init__(
         self,
         engine: Engine | None = None,
-        path_data_folder: str | None = None,
+        path_to_zip_file: str | None = None,
         force_download=False,
     ):
-        """
-        Initialize the DbManager with a database engine and path to the data files.
-
-        If path_to_zip_file == None, latest version IPNI will be downloaded.
+        """Initialize the DbManager with a database engine.
 
         Args:
-            engine: SQLAlchemy database engine instance.
-            path_to_zip_file (str): Path to the directory containing TSV files (unzipped).
+            engine (Engine | None): SQLAlchemy engine. If None, a default SQLite engine is created.
+            path_to_zip_file (str | None): Path to the zip file containing data. If None, uses default path.
+            force_download (bool): Whether to force download the data.
         """
         if isinstance(engine, Engine):
             self.engine = engine
         else:
             self.engine = create_engine(DB_DEFAULT_CONNECTION_STR)
         self.Session = sessionmaker(bind=self.engine)
-        self.path_data_folder = path_data_folder
+        self.path_to_zip_file = path_to_zip_file or PATH_TO_ZIP_FILE
         self.force_download = force_download
 
     @property
@@ -89,18 +93,18 @@ class DbManager:
         """
         return self.Session()
 
-    def create_db(self) -> None:
-        """Create all tables in the database."""
-        Base.metadata.create_all(self.engine)
+    def _set_path_to_zip_file(self, path_to_zip_file: str) -> None:
+        """Set the path to the zip file. Mainly for testing purposes.
 
-    def drop_db(self) -> None:
-        """Drop all tables from the database."""
-        Base.metadata.drop_all(self.engine)
+        Args:
+            path_to_zip_file (str): Path to the zip file.
+        """
+        self.path_to_zip_file = path_to_zip_file
 
     def recreate_db(self) -> None:
         """Recreate the database by dropping and creating all tables."""
-        self.drop_db()
-        self.create_db()
+        Base.metadata.drop_all(self.engine)
+        Base.metadata.create_all(self.engine)
         logger.info("Database recreated.")
 
     def import_data(
@@ -116,86 +120,196 @@ class DbManager:
         Returns:
             Dict[str, int]: table=key and number of inserted=value
         """
-        self.create_db()
-        imported = {}
-        if not self.path_data_folder or force_download:
-            self.path_data_folder = download_and_unzip(self.force_download)
-
         self.recreate_db()
+        imported = {}
 
-        for tsv_file, model in file_table_map.items():
-            logger.info(f"Start import into {model.__tablename__}")
-            number_of_imported_rows = self.import_model_data(tsv_file, model)
-            if not number_of_imported_rows is None:
-                imported[model.__tablename__] = number_of_imported_rows
+        if force_download or not os.path.exists(PATH_TO_TAXTREE_ZIP_FILE):
+            os.makedirs(TAXTREE_DATA_FOLDER, exist_ok=True)
+            urllib.request.urlretrieve(TAXTREE_DOWNLOAD_URL, PATH_TO_TAXTREE_ZIP_FILE)
+
+        # NCBI Taxonomy
+        # =============================================================================
+        logger.info("Loading NCBI Taxonomy data for mapping families and names")
+        with zipfile.ZipFile(PATH_TO_TAXTREE_ZIP_FILE, "r") as z:
+            with z.open("rankedlineage.dmp") as f:
+                df = pd.read_csv(
+                    f,
+                    sep=r"\t\|\t|\t\|$",
+                    header=None,
+                    usecols=[0, 1, 7],
+                    names=RANKED_LINEAGE_COLUMNS,
+                    engine="python",
+                    index_col=False,
+                    dtype=RANKED_LINEAGE_DTYPES,
+                )
+                df_tax = df[df["phylum"] == "Streptophyta"][["tax_id", "tax_name"]]
+                df = None  # free memory
+
+        # IPNI
+        # =============================================================================
+
+        if force_download or not os.path.exists(self.path_to_zip_file):
+            os.makedirs(DATA_FOLDER, exist_ok=True)
+            urllib.request.urlretrieve(DOWNLOAD_URL, self.path_to_zip_file)
+            logger.info(f"{DOWNLOAD_URL} downloaded to {self.path_to_zip_file}")
+
+        # -----------------------------------------------------------------------------
+        # Reference
+        # -----------------------------------------------------------------------------
+        logger.info("Importing references")
+        df_reference = self.get_dataframe(TsvFileName.REFERENCE, Reference)
+        df_reference.to_sql(
+            Reference.__tablename__, self.engine, if_exists="append", index=False
+        )
+        imported[Reference.__tablename__] = len(df_reference)
+        df_reference = None  # free memory
+
+        # -----------------------------------------------------------------------------
+        # Family
+        # -----------------------------------------------------------------------------
+        logger.info("Importing families")
+        with zipfile.ZipFile(self.path_to_zip_file, "r") as z:
+            with z.open("Taxon.tsv") as f:
+                df_nameid_family = (
+                    pd.read_csv(
+                        f,
+                        sep="\t",
+                        usecols=["col:family", "col:nameID"],
+                    )
+                    .dropna()
+                    .drop_duplicates()
+                    .rename(columns={"col:family": "family", "col:nameID": "id"})
+                )
+                # df_nameid_family: columns = family, id (foreign key to Name)
+
+        # create a DataFrame for Family table
+        # 1. get unique families
+        # 2. map to NCBI tax_id
+        # 3. insert into Family table
+        df_family = df_nameid_family[["family"]].dropna().drop_duplicates()
+        df_family = (
+            df_family.merge(df_tax, left_on="family", right_on="tax_name", how="left")[
+                ["family", "tax_id"]
+            ]
+            .reset_index(drop=True)
+            .rename_axis("id")
+            .rename(index=lambda i: i + 1)
+        )
+        df_family.to_sql(
+            Family.__tablename__, self.engine, if_exists="append", index=True
+        )
+        imported[Family.__tablename__] = len(df_family)
+        # -----------------------------------------------------------------------------
+        # Name
+        # -----------------------------------------------------------------------------
+        logger.info("Importing names")
+        df_family["family_id"] = df_family.index  # add foreign key for merging
+        df_name_family = df_nameid_family.merge(
+            df_family,
+            how="inner",
+            on="family",
+        )[
+            ["id", "family_id"]
+        ]  # foreign key to Name (id) and Family (family_id)
+        df_nameid_family = None  # free memory
+        df_family = None  # free memory
+
+        # -----------------------------------------------------------------------------
+        # link is removed from Name model, because it is redundant (https://ipni.org/n/{id})
+        df_name = self.get_dataframe(TsvFileName.NAME, Name).drop(columns=["link"])
+        df_name = df_name.merge(
+            df_name_family,
+            how="left",
+            on="id",
+        )
+        df_name_family = None  # free memory
+        df_name = df_name.merge(
+            df_tax, left_on="scientific_name", right_on="tax_name", how="left"
+        ).drop(columns=["tax_name"])
+        df_name["family_id"] = df_name["family_id"].astype("Int64")  # allow nulls
+        df_name["tax_id"] = df_name["tax_id"].astype("Int64")  # allow nulls
+        df_name.to_sql(Name.__tablename__, self.engine, if_exists="append", index=False)
+        imported[Name.__tablename__] = len(df_name)
+        df_name = None  # free memory
+
+        # -----------------------------------------------------------------------------
+        # TypeMaterial
+        # -----------------------------------------------------------------------------
+        logger.info("Importing type materials")
+        df_type_material = self.get_dataframe(TsvFileName.TYPE_MATERIAL, TypeMaterial)
+        df_location = (
+            df_type_material[["locality", "latitude", "longitude"]]
+            .drop_duplicates()
+            .reset_index(drop=True)
+            .rename_axis("id")
+            .rename(index=lambda i: i + 1)
+        )
+        df_location.to_sql(
+            Location.__tablename__, self.engine, if_exists="append", index=True
+        )
+        df_location["location_id"] = df_location.index  # add location_id for merging
+        df_type_material = df_type_material.merge(
+            df_location,
+            how="left",
+            on=["locality", "latitude", "longitude"],
+        ).drop(
+            columns=["locality", "latitude", "longitude"]
+        )  # drop columns after merging
+        df_location = None  # free memory
+        df_type_material.to_sql(
+            TypeMaterial.__tablename__, self.engine, if_exists="append", index=False
+        )
+        imported[TypeMaterial.__tablename__] = len(df_type_material)
+        df_type_material = None  # free memory
+        # -----------------------------------------------------------------------------
+        # NameRelation
+        # -----------------------------------------------------------------------------
+        logger.info("Importing name relations")
+        df_name_relation = self.get_dataframe(TsvFileName.NAMES_RELATION, NameRelation)
+        df_name_relation.to_sql(
+            NameRelation.__tablename__, self.engine, if_exists="append", index=False
+        )
+        imported[NameRelation.__tablename__] = len(df_name_relation)
+        df_name_relation = None  # free memory
 
         if not keep_files:
-            shutil.rmtree(self.path_data_folder)
-            shutil.rmtree(DEFAULT_PATH_UNZIPPED_DATA_FOLDER)
+            shutil.rmtree(self.path_to_zip_file)
 
-        logger.info("Data imported: %s", imported)
         return imported
 
-    def import_model_data(self, tsv_file: str, model) -> int | None:
-        """
-        Imports data from a TSV file into the specified database model.
-        Reads a TSV file, optionally processes the data based on the model type,
-        cleans and standardizes the DataFrame, and appends the data to the corresponding
-        database table.
-        Args:
-            tsv_file (str): The name of the TSV file to import.
-            model: The SQLAlchemy model class representing the target database table.
-        Returns:
-            int | None: The number of rows inserted into the database, or None if the operation fails.
-        Raises:
-            FileNotFoundError: If the data folder is not set or does not exist.
-        """
-
-        if self.path_data_folder:
-            file_path = os.path.join(self.path_data_folder, tsv_file)
-
-            df = pd.read_csv(file_path, sep="\t", low_memory=False)
-            if model == TypeMaterial:
-                df.drop(columns=["col:ID"], inplace=True)
-                df["col:remarks"] = df["col:remarks"].replace(float("nan"), None)
-                df["col:date"] = df["col:date"].map(parse_date)  # type: ignore
-
-            df = get_cleaned_and_standardized_dataframe(df)
-
-            if model == NameRelation:
-                # For NameRelation, we need to ensure that the related_name_id and name_id
-                # have a corresponding entry in the Name table.
-                df_name_id = pd.read_csv(
-                    os.path.join(self.path_data_folder, TsvFileName.NAME),
+    def get_dataframe(self, tsv_file: str, model) -> pd.DataFrame:
+        with zipfile.ZipFile(self.path_to_zip_file, "r") as z:
+            with z.open(tsv_file) as f:
+                df: pd.DataFrame = pd.read_csv(
+                    f,
                     sep="\t",
-                    usecols=["col:ID"],
-                ).rename(columns={"col:ID": "name_id"})
-                df = df[
-                    df.related_name_id.isin(df_name_id.name_id)
-                    & df.name_id.isin(df_name_id.name_id)
-                ]
+                    low_memory=False,
+                )
+        if model == TypeMaterial:
+            df.drop(columns=["col:ID"], inplace=True)
+            df["col:remarks"] = df["col:remarks"].replace(float("nan"), None)
+            df["col:date"] = df["col:date"].map(parse_date)  # type: ignore
 
-            # if model == Reference:
-            #     # For Reference, we need to ensure that the names in the reference
-            #     # have a corresponding entry in the Name table.
-            #     df_name_id = pd.read_csv(
-            #         os.path.join(self.path_data_folder, TsvFileName.NAME),
-            #         sep="\t",
-            #         usecols=["col:ID"],
-            #     ).rename(columns={"col:ID": "name_id"})
-            #     df = df[df.name_id.isin(df_name_id.name_id)]
+        df = get_cleaned_and_standardized_dataframe(df)
 
-            return df.to_sql(
-                model.__tablename__,
-                self.engine,
-                if_exists="append",
-                index=False,
-                chunksize=100000,
-            )
+        if model == NameRelation:
+            # For NameRelation, we need to ensure that the related_name_id and name_id
+            # have a corresponding entry in the Name table.
+            with zipfile.ZipFile(self.path_to_zip_file, "r") as z:
+                with z.open(TsvFileName.NAME) as f:
+                    df_name_id = pd.read_csv(
+                        f,
+                        sep="\t",
+                        usecols=["col:ID"],
+                    ).rename(columns={"col:ID": "name_id"})
+            df = df[
+                df.related_name_id.isin(df_name_id.name_id)
+                & df.name_id.isin(df_name_id.name_id)
+            ]
+        if not df.empty:
+            return df
         else:
-            raise FileNotFoundError(
-                "No import folder exists. Init with `auto_load_data=True` or set `path_data_folder`"
-            )
+            raise ValueError(f"No data found in {tsv_file}")
 
 
 def import_data(
